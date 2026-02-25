76f69f62a1424866e8005fbd023a470008d84cd5
Thiago <yumblat123@gmail.com>
2026-02-25 08:25:52 -0500

    first commit

---
+++ semana_8/mision.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string sustantivo;
+    string verbo;
+
+    cout << "Ingresa un sustantivo:  ";
+    cin >> sustantivo;
+    cout<<"Ingresa un verbo: ";
+    cin >> verbo;
+    cout << "!El  " << sustantivo <<  " decidio " << verbo << " en la luna" <<endl;
+}
